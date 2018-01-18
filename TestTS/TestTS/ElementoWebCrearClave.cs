a9bcf615280b2b2ad47175e0de8fe9fd43efcdbb
amoreno <aem.gamboa@gmail.com>
2018-01-18 14:49:26 -0300

    Se agrega CrearClave para automatización

---
+++ TestTS/TestTS/ElementoWebCrearClave.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTS
+{
+    class ElementoWebCrearClave
+    {
+        public ElementoWebCrearClave()
+        {
+            PageFactory.InitElements(PropiedadColeccionDriver.driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "rut2")]
+        public IWebElement txtRutClienteCrearClave { get; set; }
+
+        [FindsBy(How = How.Id, Using = "n_serie")]
+        public IWebElement txtNumeroSerie { get; set; }
+
+    }
+}
